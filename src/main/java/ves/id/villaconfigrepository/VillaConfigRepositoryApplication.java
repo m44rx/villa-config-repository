85dcd323e6feb7d9788f12da448f7d5f6946d720
Eddie Mallma Alvarez <emallmaa@emeal.nttdata.com>
2023-01-05 12:00:11 -0500

    first commit

---
+++ src/main/java/ves/id/villaconfigrepository/VillaConfigRepositoryApplication.java
@@ -0,0 +1,13 @@
+package ves.id.villaconfigrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillaConfigRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillaConfigRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ves/id/villaconfigrepository/VillaConfigRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ves.id.villaconfigrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillaConfigRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
